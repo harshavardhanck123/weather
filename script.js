d39f3a3794f2e8f11e30c3dd7d99e1e311e505e1
harshavardhanck123 <ckharshavardhan00@gmail.com>
2024-04-04 20:23:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+var res = fetch(" https://restcountries.com/v3.1/all")
+res.then((data) => data.json()).then((data1) => foo(data1))
+
+var contain = document.createElement("div")
+contain.className = "container"
+
+var row = document.createElement("div")
+row.className = "row"
+
+
+
+function foo(data1) {
+    for(var i=0;i<data1.length;i++)
+    {
+    var column = document.createElement("div")
+    column.className = "col-md-4"
+    column.innerHTML=`<div class="card">
+    <h5 class="card-header text-center">${names}</h5>
+    <img class="card-img-top" src="${flag}" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Capital: ${cap}</h5>
+      <p class="card-text">Population: ${pop} <br> Region: ${re}</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>`
+  var names=data1[i].name.common
+  var cap=data1[i].capital
+  var pop=data1[i].population
+  var re=data1[i].region
+  var flag=data1[i].flags.png
+  row.append(column)
+  contain.append(row)
+  document.body.append(contain)
+    }
+   
+}
