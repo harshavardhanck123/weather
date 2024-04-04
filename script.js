1b96089dfd9cd1b2cfa89cc14b5b298ddd5fbe02
harshavardhanck123 <ckharshavardhan00@gmail.com>
2024-04-04 20:28:53 +0530

    update

--- script.js
+++ script.js
@@ -12,6 +12,12 @@ row.className = "row"
 function foo(data1) {
     for(var i=0;i<data1.length;i++)
     {
+        var names=data1[i].name.common
+        var cap=data1[i].capital
+        var pop=data1[i].population
+        var re=data1[i].region
+        var flag=data1[i].flags.png
+
     var column = document.createElement("div")
     column.className = "col-md-4"
     column.innerHTML=`<div class="card">
@@ -20,14 +26,10 @@ function foo(data1) {
     <div class="card-body">
       <h5 class="card-title">Capital: ${cap}</h5>
       <p class="card-text">Population: ${pop} <br> Region: ${re}</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <a href="#" class="btn btn-primary">click for weather</a>
     </div>
   </div>`
-  var names=data1[i].name.common
-  var cap=data1[i].capital
-  var pop=data1[i].population
-  var re=data1[i].region
-  var flag=data1[i].flags.png
+
   row.append(column)
   contain.append(row)
   document.body.append(contain)
