26a1696de07b1494e7224e99934ef288e359607f
harshavardhanck123 <ckharshavardhan00@gmail.com>
2024-04-05 18:40:20 +0530

    Changed

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ var contain = document.createElement("div")
 contain.className = "container"
 
 var row = document.createElement("div")
-row.className = "row"
-
-
+row.className = "row gy-4"
 
 function foo(data1) {
     for(var i=0;i<data1.length;i++)
@@ -20,10 +18,12 @@ function foo(data1) {
 
     var column = document.createElement("div")
     column.className = "col-md-4"
-    column.innerHTML=`<div class="card">
+    column.innerHTML=`<div class="card mt-3">
     <h5 class="card-header text-center">${names}</h5>
-    <img class="card-img-top" src="${flag}" alt="Card image cap">
-    <div class="card-body">
+    <div class="d-flex justify-content-center align-items-center" style="height: 200px;">
+          <img class="card-img-top img-fluid" src="${flag}" alt="Card image cap">
+        </div>
+    <div class="card-body text-center">
       <h5 class="card-title">Capital: ${cap}</h5>
       <p class="card-text">Population: ${pop} <br> Region: ${re}</p>
       <a href="#" class="btn btn-primary">click for weather</a>
@@ -34,5 +34,4 @@ function foo(data1) {
   contain.append(row)
   document.body.append(contain)
     }
-   
 }
