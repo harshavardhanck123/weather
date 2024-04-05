fb1037d820e42b88f2dd3c683f34b213f612b263
harshavardhanck123 <ckharshavardhan00@gmail.com>
2024-04-05 19:28:37 +0530

    Style

--- script.js
+++ script.js
@@ -19,18 +19,17 @@ row.className = "row gy-4"
         var pop=data1[i].population
         var re=data1[i].region
         var flag=data1[i].flags.png
-
+        var country=data1[i].cioc
     var column = document.createElement("div")
     column.className = "col-md-4"
-    column.innerHTML=`<div class="card mt-3">
-    <h5 class="card-header text-center">${names}</h5>
-    <div class="d-flex justify-content-center align-items-center" style="height: 200px;">
-          <img class="card-img-top img-fluid" src="${flag}" alt="Card image cap">
+    column.innerHTML=`<div class="card mt-3" >
+    <h5 class="card-header text-center" style="background-color:black;color:white;" >${names}</h5>
+    <div class="d-flex justify-content-center align-items-center" style="height: 200px;background-image:linear-gradient(to right,#BDB294,#58655E)">
+          <img class="card-img-top img-fluid " src="${flag}" alt="Card image cap">
         </div>
-    <div class="card-body text-center">
-      <h5 class="card-title">Capital: ${cap}</h5>
-      <p class="card-text">Population: ${pop} <br> Region: ${re}</p>
-      <a href="#" class="btn btn-primary">click for weather</a>
+    <div class="card-body text-center" style="background-image:linear-gradient(to right,#BDB294,#58655E);color:white">
+      <p class="card-text">Capital: ${cap}  <br> Region: ${re}<br> Country Code: ${country}</p>
+      <a href="#" class="btn btn-primary" style="background-color: transparent; color: inherit; border-color: inherit;">Click for Weather</a>
     </div>
   </div>`
 
