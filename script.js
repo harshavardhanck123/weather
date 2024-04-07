3ca3b0b66e2c5c94e41e9aa5b9c272c4ed49f436
harshavardhanck123 <ckharshavardhan00@gmail.com>
2024-04-07 08:56:21 +0530

    Final

--- script.js
+++ script.js
@@ -16,10 +16,11 @@ row.className = "row gy-4"
     {
         var names=data1[i].name.common
         var cap=data1[i].capital
-        var pop=data1[i].population
         var re=data1[i].region
         var flag=data1[i].flags.png
         var country=data1[i].cioc
+        var lat = data1[i].latlng[0];
+        var lon = data1[i].latlng[1];
     var column = document.createElement("div")
     column.className = "col-md-4"
     column.innerHTML=`<div class="card mt-3" style="border: 2px solid white;" >
@@ -29,7 +30,7 @@ row.className = "row gy-4"
         </div>
     <div class="card-body text-center" style="background-image:linear-gradient(to right,#BDB294,#58655E);color:white">
       <p class="card-text">Capital: ${cap}  <br> Region: ${re}<br> Country Code: ${country}</p>
-      <a href="#" class="btn btn-primary" style="background-color: transparent; color: inherit; border-color: inherit;">Click for Weather</a>
+      <a href="#" class="btn btn-primary weather-btn" data-lat="${lat}" data-lon="${lon}" style="background-color: transparent; color: inherit; border-color: inherit;">Click for Weather</a>
     </div>
   </div>`
 
@@ -37,4 +38,23 @@ row.className = "row gy-4"
   contain.append(row)
   document.body.append(contain)
     }
+    var weatherButtons = document.querySelectorAll('.weather-btn');
+    weatherButtons.forEach(function(button) {
+        button.addEventListener('click', async function() {
+            var lat = this.getAttribute('data-lat');
+            var lon = this.getAttribute('data-lon');
+            var temperature = await Weather(lat, lon);
+            var cardBody = this.parentElement;
+            var temperatureElement = document.createElement('p');
+            temperatureElement.textContent = `Temperature: ${temperature}°C`;
+            cardBody.replaceChild(temperatureElement, this);
+        });
+    });
 }
+
+async function Weather(lat,lon,button){
+  let data=await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=3fdb46d68715c1511c6c2a05647e4ab9`)
+  let data1=await data.json()
+  return data1.main.temp;
+}
+
